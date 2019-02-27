c187341f79470b6c46350579711350d1515542fc
Josh Minzner <josh@trialspark.com>
2019-02-27 11:00:18 -0500

    Add project scaffold

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:jest/recommended',
+  ],
+  globals: {},
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {},
+  overrides: [
+    {
+      files: ['*.spec.ts'],
+      env: {
+        'jest/globals': true,
+      },
+    },
+  ],
+};
