786232b5c64db1843952473b2a82d06cfdcd7fc4
Josh Minzner <josh@trialspark.com>
2019-02-27 15:52:13 -0500

    Disallow optional properties

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { DeepRequired } from 'utility-types';
 import ObjectTracker from './ObjectTracker';
 
-export type ToString<T extends object> = (getter: (source: T) => unknown) => string;
+export type ToString<T extends object> = (getter: (source: DeepRequired<T>) => unknown) => string;
 
-export default function getterToString<T extends object>(source: T): ToString<T> {
+export default function getterToString<T extends object>(source: DeepRequired<T>): ToString<T> {
   const tracker = new ObjectTracker(source);
 
   return getter => {
